73b42c61f6f7ac3791f9b627c1d9181c2357a17b
Alvinbsi <salvinselva@gmail.com>
2024-12-22 00:42:32 +0530

    6

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,16 @@ module "eks" {
   cluster_name    = "alvin-eks-cluster"
   cluster_version = "1.27"
 
+  node_group_defaults = {
+    ami_type = "AL2_x86_64"
+    instance_type = "t3.medium"
+  }
+
   node_groups = {
     default = {
       desired_capacity = 2
       max_size         = 3
       min_size         = 1
-      instance_type    = "t3.medium"
     }
   }
 }

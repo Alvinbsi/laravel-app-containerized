d54afca77f7bea2345583c2580438088de4f50c2
Alvinbsi <salvinselva@gmail.com>
2024-12-22 00:27:19 +0530

    third commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-variable "region" {
-  default = "ap-south-1"
+output "eks_cluster_name" {
+  value = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,3 @@
 variable "region" {
   default = "ap-south-1"
 }
-```
-
-3. **`outputs.tf`**:
-```hcl
-output "eks_cluster_name" {
-  value = module.eks.cluster_id
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}

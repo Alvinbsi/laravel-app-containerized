2bab306379c26ef3b90acae4846065b2d34adf70
Alvinbsi <salvinselva@gmail.com>
2024-12-22 08:14:05 +0530

    7

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,14 @@ provider "aws" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.0" # Specify a compatible version
   cluster_name    = "alvin-eks-cluster"
   cluster_version = "1.27"
-
-  node_group_defaults = {
-    ami_type = "AL2_x86_64"
-    instance_type = "t3.medium"
-  }
-
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_size         = 2
-      min_size         = 1
+  worker_groups = [
+    {
+      instance_type = "t3.medium"
+      asg_desired_capacity = 2
+      asg_min_size         = 1
+      asg_max_size         = 2
     }
-  }
+  ]
 }
